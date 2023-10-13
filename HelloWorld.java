70d4fa56d940f679796122f24689e51fcfd207ab
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-13 17:36:15 -0300

    feat: add data types

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,6 +9,19 @@ public class HelloWorld {
     // Todo conteudo devera ser inserido aqui dentro
 
     public static void main(String[] args) {
+        /**
+         * Tipos de dados
+         * Numeros (int, double, float, long)
+         * Texto (String)
+         * Boleano (boolean)
+         */
+
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 10.0;
+        float dadoDoTipoFloat = 10.0f;
+        long dadoDoTipoLong = 10L;
+        String dadoDoTipoString = "Hello World";
+        boolean dadoDoTipoBoolean = true;
         
     }
 
