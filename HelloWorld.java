604c00545ac172a7a27f7a0917cd12e71348f399
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-13 17:14:26 -0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * HelloWorld = Nome da minha classe
+ * public = Tipo de acesso da classe
+ * class = Tipo da classe
+ */
+public class HelloWorld {
+
+    // Todo conteudo devera ser inserido aqui dentro
+    
+}
+
+// Fora do escopo da classe
