c41eb64f771b37176787f21177cbb3d73f7c3413
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-14 18:14:02 -0300

    feat: create data structure

---
+++ EstruturaDeDados.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstruturaDeDados {
+    
+    public static void main(String[] args) {
+        // Lista
+
+        List<String> nomes = new ArrayList<String>();
+        nomes.add("João");
+        nomes.add("Maria");
+        nomes.add("Joaquim");
+
+        System.out.println(nomes);
+
+        // for(String nome: nomes) {
+        //     System.out.println("O nome é: " + nome);
+        // }
+
+        nomes.forEach(nome -> {
+            System.out.println("O nome é: " + nome);
+        });
+    }
+}
