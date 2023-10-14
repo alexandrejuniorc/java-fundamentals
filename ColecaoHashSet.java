0ece82c97e2787b1ed2132abcbbfccbc0eebedae
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-14 19:01:59 -0300

    feat: create HashSet

---
+++ ColecaoHashSet.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class ColecaoHashSet {
+    public static void main(String[] args) {
+        HashSet<String> nomes = new HashSet<>();
+        nomes.add("João");
+        nomes.add("Maria");
+        nomes.add("Joaquim");
+
+        System.out.println(nomes);
+
+        for(String elemento: nomes) {
+            System.out.println("O nome é: " + elemento);
+        }
+
+
+        System.out.println(nomes.contains("João"));
+    }
+}
